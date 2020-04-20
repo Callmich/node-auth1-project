cbd366ebccc20f8738391c9c980d28e53d41ee6e
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-20 14:45:27 -0600

    added in inital knex file and finished off the router info. ready to start moving into the migrations and creating the dB

--- api/server.js
+++ api/server.js
@@ -3,7 +3,12 @@ const helmet = require('helmet');
 const cors = require('cors');
 const session = require('express-session');
 
-//Set Routers & Middleware
+//Set Routers 
+const usersRouter = require('../users/users-router.js');
+const authRouter = require('../auth/auth-router.js');
+const loginRouter = require('../login/login-router.js');
+
+//Set Middleware Authenticator
 
 
 const server = express();
@@ -20,6 +25,7 @@ server.use(cors());
 
 server.use("/api/users", authenticator, usersRouter);
 server.use("/api/auth", authRouter)
+server.use("/api/login", loginRouter)
 
 server.get("/", (req, res) =>{
     res.status(200).json({message: `The Server is Up and Running!!!!`})
