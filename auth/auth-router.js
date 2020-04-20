acd22c4455b60ab4888d89e59ed19cb20281dc80
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-20 15:50:11 -0600

    added first pass on Auth and Login servers

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 
+const Users = require("../users/users-model.js");
+
 //set Db
+router.post("/", (req, res) =>{
+    let user = req.body;
+
+    const rounds = process.env.HASH_ROUNDS || 8;
+
+    const hash = bcrypt.hashSync(user.password, rounds);
+
+    user.password = hash;
+
+    Users.add(user)
+      .then(saved =>{
+          res.status(201).json(saved)
+      })
+      .catch(error =>{
+          res.status(500).json({ errorMessage: error.message})
+      })
+})
+
 
 //CRUD Op
 
